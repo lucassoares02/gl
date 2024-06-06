b931a9b79ff6dafd2fbcc94e8b5f0b8692840b1a
Lucas Soares <lucas.soares@viprede.com>
2024-06-06 15:44:57 -0300

    update screen register

--- lib/src/modules/register/screen.dart
+++ lib/src/modules/register/screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gl/src/components/button.dart';
+import 'package:gl/src/utils/app_spacing.dart';
+import 'package:gl/src/utils/decorations.dart';
+import 'package:gl/src/utils/spacing.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -10,11 +14,63 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text("Register"),
-        ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(appPadding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(),
+                const AppSpacing(),
+                const Text(
+                  "Registre-se",
+                  style: appTitleText,
+                ),
+                const AppSpacing(),
+                const Text("E-mail", style: appLabelText),
+                const SizedBox(height: 5),
+                TextFormField(
+                  decoration: appInputDecoration(label: "Digite seu e-mail..."),
+                ),
+                const AppSpacing(),
+                const Text("Nome", style: appLabelText),
+                const SizedBox(height: 5),
+                TextFormField(
+                  decoration: appInputDecoration(label: "Digite seu nome..."),
+                ),
+                const AppSpacing(),
+                const Text("Senha", style: appLabelText),
+                const SizedBox(height: 5),
+                TextFormField(
+                  decoration: appInputDecoration(label: "Digite sua senha..."),
+                ),
+                const AppSpacing(),
+                const Text("Confirme sua Senha", style: appLabelText),
+                const SizedBox(height: 5),
+                TextFormField(
+                  decoration: appInputDecoration(label: "Confirme sua senha..."),
+                ),
+                const AppSpacing(),
+                AppButton(label: "Registrar", onPressed: () {}),
+                const AppSpacing(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Possuí conta? Acesse"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
